2eeb8956beb63cfcdc956ce3f058c1bf73aab5bb
cronrpc <147160173+cronrpc@users.noreply.github.com>
2025-08-02 01:11:35 +0800

    update SunPostion

---
+++ Assets/Arts/Skybox/Scripts/SunPostion.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SunPostion : MonoBehaviour
+{
+    public float d = 0; // 0 = noon
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
